86389b155771a33c6aef08462b35535601236893
Carlosdvp <carlosdvp.tx@gmail.com>
2023-06-28 15:59:32 -0400

    wrapped up the initial setup with the installation of tailwindcss/forms.

--- vue-client/src/views/Home.vue
+++ vue-client/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <h1 class="text-4xl text-center my-5">Home</h1>
-  <p>{{ meals }}</p>
+  <h1 class="text-4xl font-bold text-center my-5">Recipes</h1>
+  <div class="flex flex-col p-8">
+    <input type="text" class="border-2 border-gray-200 w-[80%] mx-auto my-0" placeholder="Search for Recipes">
+    <div class="flex justify-center gap-2 pt-4">
+      <router-link to="/" v-for="letter of letters">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -8,5 +15,5 @@ import { computed } from 'vue'
 import store from '@/store'
 
 const meals = computed(() => store.state.meals);
-
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 </script>
